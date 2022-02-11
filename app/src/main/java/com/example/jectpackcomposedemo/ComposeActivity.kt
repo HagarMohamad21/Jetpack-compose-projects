d347700b59a23f7ea1e71b6c955ab7c4d9d0c8e9
Hagar Mohamed <hagar.mohamad21@gmail.com>
2022-02-11 11:19:15 +0200

    Base project for all jetpack compose projects

---
+++ app/src/main/java/com/example/jectpackcomposedemo/ComposeActivity.kt
@@ -0,0 +1,57 @@
+package com.example.jectpackcomposedemo
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.CornerSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.jectpackcomposedemo.ui.theme.JectPackComposeDemoTheme
+
+class ComposeActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Surface(
+                modifier = Modifier.padding(12.dp)
+            ) {
+
+            }
+
+        }
+    }
+}
+
+
+@Composable
+fun Content(container: @Composable () -> Unit) {
+    JectPackComposeDemoTheme {
+        // A surface container using the 'background' color from the theme
+        Surface(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(all = 10.dp),
+            color = MaterialTheme.colors.background
+        ) {
+            container()
+        }
+    }
+}
+
+
+
