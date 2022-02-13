790a06837673776edcc817b197be98c1f5a48df8
Hagar Mohamed <hagar.mohamad21@gmail.com>
2022-02-13 13:27:13 +0200

    added slider

--- app/src/main/java/com/example/jectpackcomposedemo/ComposeActivity.kt
+++ app/src/main/java/com/example/jectpackcomposedemo/ComposeActivity.kt
@@ -9,6 +9,7 @@ import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Slider
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
@@ -18,6 +19,8 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.Alignment.Companion.CenterHorizontally
+import androidx.compose.ui.Alignment.Companion.CenterVertically
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -33,6 +36,7 @@ import androidx.compose.ui.unit.sp
 import com.example.jectpackcomposedemo.components.CustomInputField
 import com.example.jectpackcomposedemo.ui.theme.JectPackComposeDemoTheme
 import com.example.jectpackcomposedemo.widgets.RoundIconButton
+import javax.sql.RowSet
 
 
 class ComposeActivity : ComponentActivity() {
@@ -126,9 +130,19 @@ fun BillForm(modifier: Modifier=Modifier,onBillChange:(String)->Unit){
     val valid= remember(totalBill.value) {
         totalBill.value.trim().isNotEmpty()
     }
-
+val people= remember { mutableStateOf(1)}
     val keyboardController=LocalSoftwareKeyboardController.current
 
+ val tipAmount= remember {
+     mutableStateOf(0f)
+ }
+
+ val percentage= remember {
+     mutableStateOf(0f)
+ }
+val sliderValue= remember {
+    mutableStateOf(0f)
+}
     CustomInputField(isSingleLine = true, valueState = totalBill, labelId = stringResource(R.string.enter_bill) , enabled =true,
         onAction = KeyboardActions{
             if(!valid){
@@ -137,7 +151,7 @@ fun BillForm(modifier: Modifier=Modifier,onBillChange:(String)->Unit){
             onBillChange(totalBill.value.trim())
             keyboardController?.hide()
         })
-    if(valid){
+//    if(valid){
         Row(
             modifier = Modifier.padding(all=3.dp),
             horizontalArrangement = Arrangement.Start
@@ -147,16 +161,41 @@ fun BillForm(modifier: Modifier=Modifier,onBillChange:(String)->Unit){
             ) )
             Spacer(modifier = Modifier.width(120.dp))
             Row(modifier = Modifier.padding(all = 3.dp), horizontalArrangement = Arrangement.End){
-
-                RoundIconButton(imageVector = Icons.Default.Remove, onClick = {  })
-                RoundIconButton(imageVector = Icons.Default.Add, onClick = {  })
+                RoundIconButton(imageVector = Icons.Default.Remove, onClick = {
+                    if(people.value>1){
+                        people.value--
+                    }
+                })
+                Text(text="${people.value}", modifier = Modifier
+                    .align(CenterVertically)
+                    .padding(start = 9.dp, end = 9.dp))
+                RoundIconButton(imageVector = Icons.Default.Add, onClick = {
+                    people.value++
+                })
 
             }
+
         }
 
-       
+    Row(modifier = Modifier
+        .padding(all = 3.dp)
+        .fillMaxWidth()){
+        Text(text = "Tip:", modifier = Modifier.align(alignment = CenterVertically))
+       Row( modifier = Modifier
+           .fillMaxWidth()
+           .padding(end = 5.dp), horizontalArrangement = Arrangement.End){ Text(text = "$${tipAmount.value}")}
     }
-    else Box(){}
+    Column(modifier = Modifier
+        .fillMaxWidth()
+        .padding(all = 10.dp)) {
+        Text(modifier = Modifier.align(CenterHorizontally), text = "%${percentage.value}")
+        Spacer(modifier = Modifier.height(14.dp))
+        Slider(value = sliderValue.value, onValueChange ={
+            sliderValue.value=it
+        }, steps = 5)
+    }
+//    }
+//    else Box(){}
 }
 
 
